811a3c4e27d7f4476e0b8cdebe0abbe09a7505ab
TomOnderwater <t.c.onderwater@gmail.com>
2020-10-23 01:57:15 +0200

    je kan lekker rond wankelen, heb wat basis bedacht voor multiplayer handling

--- public/js/main.js
+++ public/js/main.js
@@ -1,49 +1,93 @@
-let screen
-let enivornment
-let myBlob
-
 const fire = 0, water = 1, earth = 2, air = 3
 
+let camera
+let environment, screem
+let blobs = []
+let myblob
+let myid
+
+//socket connection:
+let socket
+
 function setup() {
-  screen = createCanvas(600, 600)
+  camera = {focus: createVector(0, 0), zoom: 1}
+  screen = createCanvas(windowWidth, windowHeight)
   environment = new Environment()
-  myBlob = new Blob(0, createVector(0, 0))
+  
+  //begin the socket here
+
+
+  //get a player id from the server
+  myid = getPlayerId()
+  myid.then((id) => {
+    blobs.push(new Blob(player = id, pos = createVector(0, 0)))
+    console.log(blobs)
+    console.log(id)
+    for (let b of blobs) {
+      if (b.player === id)
+        {
+          myblob = b
+          break
+        }
+    }
+    console.log('id: ', myid)
+    console.log('my blob status: ', myblob)
+    updateCamera()
+  })
+
+  //define functions to update the blocks based on server data here
+
+  //update a blob list
+
+  //
+
 }
 
 function draw() {
   environment.draw()
-  //get the user input
-  getInput()
-
-  myBlob.draw()
-
-  movePos(myBlob.move(getInput()))
+  myblob.update()
+  blobs.forEach(blob => blob.draw())
+  updateCamera()
 }
 
-function getPos(pos) {
-  return environment.getPos(pos)
+function updateblobs(newblobs) {
+  blobs = readbloblist(newblobs)
 }
-function movePos(pos) {
-  environment.update(pos)
+function readbloblist(blobs) {
+  // should be a js object of the form:
+  // blobs = [{id:[array positions], elements: [array]}]
+
+
+  //could clear the array and refill it
+
+  //could 
+
+}
+
+function createbloblist() {
+  let list = []
+  blobs.forEach(blob => {
+    list.push({id: blob.player, elements: blob.getElements()})
+  })
+}
+
+async function getPlayerId() {
+  //should be calling the server for an assigned id
+    return new Promise( ( resolve ) => {
+      resolve( 0 )
+    } )
+}
+
+//server function calling readbloblist(blobs)
+function updateblobs() {
+
 }
 
 function getInput() {
-  let a = myBlob.getPos()
-  let b = environment.getFocus()
-  drawCircle(b, color(255, 0, 255))
-  let dir = p5.Vector.sub(b, a)
-  dir.mult(0.1)
-  drawCircle(dir, color(0, 255, 0))
-  //console.log(dir)
+  let dir = halfScreen().sub(createVector(mouseX, mouseY))
+  // environment.drawCircle(dir, color(0, 255, 0))
   return dir
-}
-
-function drawCircle(pos, color) {
-  let p = environment.getPos(pos)
-  push()
-  fill(color)
-  circle(p.x, p.y, 30)
-  pop()
+  //createVector(1, 0)
 }
 
 class Blob {
@@ -59,14 +103,34 @@ class Blob {
     return this.pos
   }
 
+  getElements() {
+    return this.elements
+  }
+
   draw() {
     this.elements.forEach(element => element.draw())
   }
 
-  move(amount) {
-    this.elements.forEach(element => element.move(amount))
-    return this.getPos()
+  update() {
+    //console.log(this.pos)
+    //console.log(getFocus(this.pos))
+    //only update if it's our blob
+      let userinput = getInput()
+      userinput.mult(0.01)
+      this.elements.forEach(element => element.move(userinput))
+      this.calcAveragePos()
+      return this.getPos()
   }
+
+  calcAveragePos() {
+    let avg = createVector(0, 0)
+    for (let e of this.elements) {
+      avg.add(e.getPos())
+    }
+    avg.mult(this.elements.length)
+    this.pos = avg
+  }
+
   addElement(elem) {
     this.elements.push(elem)
   }
@@ -78,14 +142,23 @@ class Element {
     this.pos = pos
   }
   draw() {
-    push()
-      fill(255, 0 ,0)
-
-      let pos = getPos(this.pos)
-      circle(pos.x, pos.y, 30)
-    pop()
+    //
+    //reference p:
+    let p = getFocus(this.pos)
+    if (inbounds(p)) {
+      push()
+        fill(255, 0, 0)
+        circle(p.x, p.y, 30)
+      pop()
+    }
   }
 
+  getWeight() {
+    return 1
+  }
+  getPos() {
+    return this.pos
+  }
   move(amount) {
     this.pos.add(amount)
   }
