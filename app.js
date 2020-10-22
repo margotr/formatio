593ec9e3d7be9fc3208f24bded7c7a7316dbb4bd
Margot Rutgers <margot@elnino.tech>
2020-10-22 21:46:50 +0200

    some stuffs

--- app.js
+++ app.js
@@ -10,5 +10,5 @@ app.get("/", function (req, res) {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Formatio listening at http://localhost:${port}`);
 });
