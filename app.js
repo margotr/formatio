3800f228a71125f35e639a0de8081c7be3425a71
Margot Rutgers <margot@elnino.tech>
2020-10-22 21:19:06 +0200

    Add viewable view

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
+var path = require("path");
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/views/index.html"));
 });
 
 app.listen(port, () => {
