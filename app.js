7ebd176904c74bae940dd582a91f1e3a0913a36d
Margot Rutgers <margot@elnino.tech>
2020-10-29 20:18:45 +0100

    pretty landing page

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const express = require("express");
 const app = express();
 var path = require("path");
 const port = 3000;
-const socket = require('socket.io')
+const socket = require("socket.io");
+const bodyParser = require("body-parser");
+
+var dogsArr = [];
+app.use(bodyParser.json());
 
 //this should be moved to a seperate process to clear processing power
 //for now it's fine
-const gameEnv = require('./game.js')
+const gameEnv = require("./game.js");
 
 app.use(express.static("public"));
 
@@ -15,53 +19,57 @@ app.get("/", function (req, res) {
 });
 
 app.get("/landing", function (req, res) {
-  res.sendFile(path.join(__dirname + "/public/landing.html"));
+    res.sendFile(path.join(__dirname + "/public/landing.html"));
 });
+
+app.post("/input_name", function (req, res) {
+  console.log(req);
+  res.send('hello!')
+})
 
 const server = app.listen(port, () => {
   console.log(`Formatio listening at http://localhost:${port}`);
 });
 
-const io = socket(server)
+const io = socket(server);
 
-io.sockets.on('connection', newConnection)
-const game = new gameEnv.Game()
+io.sockets.on("connection", newConnection);
+const game = new gameEnv.Game();
 
 function runGames() {
-  game.run()
-  io.sockets.emit('update', game.getGameData())
-  setTimeout(runGames, 20)
+  game.run();
+  io.sockets.emit("update", game.getGameData());
+  setTimeout(runGames, 20);
 }
-runGames()
-
+runGames();
 
 function newConnection(socket) {
   //links the client with the actual server
-  console.log('new socket connected: ', socket.id)
-  socket.on('newplayer', addPlayer) 
-  socket.on('input', handleInput)
-  socket.on('test', testfunction)
+  console.log("new socket connected: ", socket.id);
+  socket.on("newplayer", addPlayer);
+  socket.on("input", handleInput);
+  socket.on("test", testfunction);
 
   function testfunction(data) {
-    console.log(data)
+    console.log(data);
   }
 
   function addPlayer() {
-    game.addPlayer(socket.id)
-    socket.emit('start', socket.id)
+    game.addPlayer(socket.id);
+    socket.emit("start", socket.id);
   }
   function handleInput(data) {
-    game.updatePlayerInput(socket.id, data)
+    game.updatePlayerInput(socket.id, data);
   }
 }
 
 function cleanGame() {
-    let sockets = io.sockets.sockets
-    let ids = []
-    for (let id in sockets) {
-      ids.push(id)
-    }
-    game.clean(ids)
-    setTimeout(cleanGame, 1000)
+  let sockets = io.sockets.sockets;
+  let ids = [];
+  for (let id in sockets) {
+    ids.push(id);
+  }
+  game.clean(ids);
+  setTimeout(cleanGame, 1000);
 }
-cleanGame()
+cleanGame();
